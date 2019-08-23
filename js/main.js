a83b48a5b58cbbc460c0f1379c220102e4dd5b16
bnding <bnding@gmail.com>
2019-08-23 19:12:05 -0400

    playing around with colors. more brainstorming for layout

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 
-window.Vue = Vue;
-new Vue({
-    el: "#myExperience",
-    components: {
-        'experience': httpVueLoader("../VueComponents/experience.vue")
-    }
-})
+function renderComponents() {
+    window.Vue = Vue;
+    new Vue({
+        el: "#myExperience",
+        components: {
+            'experience': httpVueLoader("../VueComponents/experience.vue")
+        }
+    })
+}
+
+function alternateSections() {
+    $(document).ready(function () {
+        var count = 0;
+        $("section").each(function (index) {
+            if (count % 2 == 0) {
+                $(this).css({"background-color": "#192024", "color": "#fff"})
+            }
+            count++;
+        })
+    })
+
+}
+
+alternateSections();
+renderComponents();
